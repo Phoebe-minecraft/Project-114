23b4c9f8e311674ee36c7776c0a7a833380f587d
Phoebe-minecraft <136461285+Phoebe-minecraft@users.noreply.github.com>
2024-04-18 16:59:11 +0800

    Add files via upload

---
+++ main.js
@@ -0,0 +1,16 @@
+function preload(){
+
+}
+
+function draw(){
+
+}
+
+function setup(){
+    canvas = createCanvas(300,300);
+    canvas.center()
+}
+
+function snapsh0t(){
+    save("image.jpg")
+}
